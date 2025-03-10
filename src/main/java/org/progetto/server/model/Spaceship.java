959e517015d6388abf6126a5a1883d572d882e80
CreepyToucan <lolloorlo@gmail.com>
2025-03-10 14:25:46 +0100

    added "Spaceship" and "BuildingBoard" clases

---
+++ src/main/java/org/progetto/server/model/Spaceship.java
@@ -0,0 +1,125 @@
+package org.progetto.server.model;
+
+public class Spaceship {
+
+    private int destroyed;
+    private int crewCount;
+    private int boxValue;
+    private int batteriesCount;
+    private int exposedConnectorsCount;
+    private boolean alienPurple;
+    private boolean alienOrange;
+    private float shootingPower;
+    private float enginePower;
+    private int[] shields;
+    private BuildingBoard buildingBoard;
+
+    //initialize spaceship with initial values of attributes//
+    public Spaceship(int destroyed,int crewCount,int boxValue,int batteriesCount,int exposedConnectorsCount,
+              boolean alienPurple,boolean alienOrange,float shootingPower, float enginePower,int[] shields) {
+
+        this.destroyed = destroyed;
+        this.crewCount = crewCount;
+        this.boxValue = boxValue;
+        this.batteriesCount = batteriesCount;
+        this.exposedConnectorsCount = exposedConnectorsCount;
+        this.alienPurple = alienPurple;
+        this.alienOrange = alienOrange;
+        this.shootingPower = shootingPower;
+        this.enginePower = enginePower;
+        this.shields = shields;
+
+    }
+
+
+
+
+    // getter //
+    public int getDestroyedCount() {
+        return destroyed;
+    }
+
+    public int getCrewCount() {
+        return crewCount;
+    }
+
+    public int getBoxValue() {
+        return boxValue;
+    }
+
+    public int getBatteriesCount() {
+        return batteriesCount;
+    }
+
+    public int getExposedConnectorsCount() {
+        return exposedConnectorsCount;
+    }
+
+    public boolean getAlienPurple() {
+        return alienPurple;
+    }
+
+    public boolean getAlienOrange() {
+        return alienOrange;
+    }
+
+    public float getShootingPower(){
+        return shootingPower;
+    }
+
+    public float getEnginePower(){
+        return enginePower;
+    }
+
+    public int[] getShields() {
+        return shields;
+    }
+
+
+    // setter //
+
+    public void setDestroyedCount(int destroyed) {
+        this.destroyed = destroyed;
+    }
+
+    public void setCrew(int quantity) {
+        crewCount = quantity;
+    }
+
+    public void setBoxValue(int value) {
+        boxValue = value;
+    }
+
+    public void setBatteriesCount(int quantity) {
+        batteriesCount = quantity;
+    }
+
+    public void setExposedConnectorsCount(int count) {
+        exposedConnectorsCount = count;
+    }
+
+    public void setAlienPurple(boolean alien) {
+        alienPurple = alien;
+    }
+
+    public void setAlienOrange(boolean alien) {
+        alienOrange = alien;
+    }
+
+    public void setShootingPower(float power) {
+        shootingPower = power;
+    }
+
+    public void setEnginePower(float power) {
+        enginePower = power;
+    }
+
+    public void setShields(int[] shields) {
+        this.shields = shields;
+    }
+
+
+
+
+
+}
