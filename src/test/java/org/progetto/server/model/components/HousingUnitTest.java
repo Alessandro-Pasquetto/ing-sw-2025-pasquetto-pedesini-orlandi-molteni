28175a23f0da9c13832a49c3c98493146a365e32
Gabriele <gabriele.pedesini@mail.polimi.it>
2025-03-21 11:07:35 +0100

    Implemented incrementCrewCount(), decrementCrewCount() tests

--- src/test/java/org/progetto/server/model/components/HousingUnitTest.java
+++ src/test/java/org/progetto/server/model/components/HousingUnitTest.java
@@ -53,9 +53,29 @@ class HousingUnitTest {
 
     @Test
     void incrementCrewCount() {
+        HousingUnit unit = new HousingUnit(ComponentType.HOUSING_UNIT, new int[]{1, 1, 1, 1}, "imgPath", 2);
+
+        // Adds crew to the housing unit
+        assertTrue(unit.incrementCrewCount(1));
+        assertEquals(1, unit.getCrewCount());
+
+        // Tries to add more crew, but it is full
+        assertFalse(unit.incrementCrewCount(2));
+        assertEquals(1, unit.getCrewCount());
     }
 
     @Test
     void decrementCrewCount() {
+        HousingUnit unit = new HousingUnit(ComponentType.HOUSING_UNIT, new int[]{1, 1, 1, 1}, "imgPath", 2);
+
+        unit.incrementCrewCount(2);
+
+        // Removes batteries from the battery storage
+        assertTrue(unit.decrementCrewCount(1));
+        assertEquals(1, unit.getCrewCount());
+
+        // Tries to remove batteries, but there aren't enough
+        assertFalse(unit.decrementCrewCount(2));
+        assertEquals(1, unit.getCrewCount());
     }
 }
